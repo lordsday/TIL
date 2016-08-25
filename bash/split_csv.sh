d1d69478e1d63d0da1051cffb4d9b001f7da5f2f
juil.ko <juil.ko@sk.com>
2016-08-25 14:33:17 +0900

    Add a script file spliting file and keeping header row

---
+++ bash/split_csv.sh
@@ -0,0 +1,11 @@
+filename=$1
+line=$2
+
+tail -n +2 $filename | split -l $line - split_
+for file in split_*
+do
+    head -n 1 $filename > tmp_file
+    cat $file >> tmp_file
+    mv -f tmp_file $file
+done
+
